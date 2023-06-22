ddf53dd4d35f9bb9555a62464d78cd45083af048
Rafael Silva de Medeiros <rafaelmdeirosfmm@gmail.com>
2023-06-22 01:48:53 -0400

    feat: Add creation date

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,11 +1,11 @@
-import React, { useLayoutEffect } from "react";
-import { Box, Button, Flex, Text, Input } from "@chakra-ui/react";
+import React, { useLayoutEffect, useState } from "react";
+import { Box, Button, Flex, Text, Input, Textarea } from "@chakra-ui/react";
 import { Draggable } from "react-beautiful-dnd";
 import { DeleteIcon } from "@chakra-ui/icons";
 
 const Card = ({ task, index, handleChange, column, deleteTask }) => {
-  const [isEditable, setIsEditable] = React.useState(false);
-  const [value, setValue] = React.useState("");
+  const [isEditable, setIsEditable] = useState(false);
+  const [value, setValue] = useState("");
 
   useLayoutEffect(() => {
     setValue(task.content);
@@ -19,7 +19,8 @@ const Card = ({ task, index, handleChange, column, deleteTask }) => {
   const handleIsEditable = (input) => {
     setIsEditable(!isEditable);
     if (input && value) {
-      handleChange(column, value, task.id);
+      task.new = false;
+      handleChange(column, value, task.id, task.creationDate);
     }
   };
 
@@ -27,6 +28,7 @@ const Card = ({ task, index, handleChange, column, deleteTask }) => {
     <Draggable key={task.id} draggableId={`${task.id}`} index={index}>
       {(draggableProvider, snapshot) => (
         <Flex
+          flexDirection="column"
           ref={draggableProvider.innerRef}
           {...draggableProvider.draggableProps}
           {...draggableProvider.dragHandleProps}
@@ -38,47 +40,60 @@ const Card = ({ task, index, handleChange, column, deleteTask }) => {
           p="1.5rem"
           justifyContent="space-between"
           position="relative"
+          gap="0.4rem"
         >
-          <Box
-            position="absolute"
-            w="40px"
-            h="8px"
-            bg="green"
-            top="0"
-            left="15px"
-            borderRadius="0 0 4px 4px"
-          />
-          {isEditable ? (
-            <Input
-              onBlur={() => handleIsEditable(true)}
-              value={value}
-              onChange={handleValue}
+          <Flex justifyContent="space-between">
+            <Box
+              position="absolute"
+              w="40px"
+              h="8px"
+              bg="green"
+              top="0"
+              left="15px"
+              borderRadius="0 0 4px 4px"
             />
-          ) : (
-            <Text
-              onClick={() => handleIsEditable(false)}
-              maxW="60%"
-              flexWrap="wrap"
-            >
-              {task?.content}
-            </Text>
-          )}
+            {isEditable || task?.new ? (
+              <Textarea
+                onBlur={() => handleIsEditable(true)}
+                value={value}
+                onChange={handleValue}
+              />
+            ) : (
+              <Text
+                backgroundColor="#2D374"
+                onClick={() => handleIsEditable(false)}
+                maxW="60%"
+                flexWrap="wrap"
+              >
+                {task?.content}
+              </Text>
+            )}
 
-          <Box
-            display="flex"
-            justifySelf="flex-end"
-            alignSelf="center"
-            flexDir="row"
-            gap="8px"
-          >
-            <Button
-              bg="delete-button"
-              _hover={{ background: "#c71212d6" }}
-              onClick={() => deleteTask(column, task.id)}
+            <Box
+              display="flex"
+              justifySelf="flex-end"
+              alignSelf="center"
+              flexDir="row"
+              gap="8px"
             >
-              <DeleteIcon />
-            </Button>
-          </Box>
+              <Button
+                bg="delete-button"
+                _focus={{ background: "#c71212d6" }}
+                _hover={{ background: "#c71212d6" }}
+                onClick={() => deleteTask(column, task.id)}
+              >
+                <DeleteIcon />
+              </Button>
+            </Box>
+          </Flex>
+          <Flex>
+            <Text fontWeight="bold" fontSize="0.8rem">
+              {console.log(typeof task?.creationDate)}
+              {new Date(task?.creationDate).getDate()} /{" "}
+              {`${new Date(task?.creationDate).getMonth()}`.padStart(2, 0)} /{" "}
+              {new Date(task?.creationDate).getFullYear()}
+            </Text>
+          </Flex>
         </Flex>
       )}
     </Draggable>

19aa91efabaf943d01725f58ac0ab600cc551619
Rafael Silva de Medeiros <rafaelmdeirosfmm@gmail.com>
2023-06-25 20:04:05 -0400

    feat: Update scroll style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../src/theme";
+import "../styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

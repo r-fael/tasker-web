5bd6730e2135f7a12cef9f3312ba9c9a750a1ce3
Rafael Silva de Medeiros <rafaelmdeirosfmm@gmail.com>
2024-05-05 18:50:05 -0400

    feat: Update delete card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,8 +1,17 @@
-import React, { useLayoutEffect, useState } from "react";
-import { Box, Button, Flex, Text, Textarea } from "@chakra-ui/react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import {
+  Box,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  Text,
+  Textarea,
+  Button,
+} from "@chakra-ui/react";
 import { Draggable } from "react-beautiful-dnd";
 import Markdown from "../Markdown/Markdown";
-import { CalendarIcon, SmallCloseIcon, TimeIcon } from "@chakra-ui/icons";
+import { CalendarIcon, DeleteIcon, TimeIcon } from "@chakra-ui/icons";
 import styles from "./Card.module.scss";
 
 const DateText = ({ date }) => {
@@ -45,19 +54,6 @@ const DateTime = ({ dateTime }) => {
         <TimeText date={dateTime} />
       </Flex>
     </Flex>
-  );
-};
-
-const DeleteButton = ({ column, task, deleteTask }) => {
-  return (
-    <Box display="flex" justifySelf="flex-end" flexDir="row" gap="8px">
-      <Box
-        className={styles.deleteButton}
-        onClick={() => deleteTask(column, task.id)}
-      >
-        <SmallCloseIcon color="white" />
-      </Box>
-    </Box>
   );
 };
 
@@ -105,6 +101,50 @@ const PriorityBox = ({ handlePriority, task }) => {
   );
 };
 
+const MenuContainer = ({ isOpen, setIsOpen, deleteTask, task, column }) => {
+  const [goToConfirm, setGoToConfirm] = useState(false);
+
+  const handleClick = () => {
+    if (!goToConfirm) {
+      setGoToConfirm(true);
+    } else {
+      deleteTask(column, task.id);
+      handleClose();
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) setGoToConfirm(false);
+  }, [isOpen]);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Menu
+      isOpen={isOpen}
+      onBlur={() => handleClose()}
+      closeOnSelect={false}
+      onClose={() => handleClose()}
+    >
+      <MenuList>
+        <MenuItem onClick={handleClick}>
+          <Button>
+            {goToConfirm ? (
+              "Confirm?"
+            ) : (
+              <>
+                <DeleteIcon fontSize="small" /> Delete Task
+              </>
+            )}
+          </Button>
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
 const Card = ({
   task,
   index,
@@ -115,6 +155,8 @@ const Card = ({
 }) => {
   const [isEditable, setIsEditable] = useState(false);
   const [value, setValue] = useState("");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const cardRef = useRef();
 
   useLayoutEffect(() => {
     setValue(task.content);
@@ -135,42 +177,51 @@ const Card = ({
 
   const creationDate = new Date(task?.creationDate);
 
+  useEffect(() => {
+    const event = cardRef.current.addEventListener("contextmenu", (event) => {
+      console.log("socorro");
+      event.preventDefault();
+      if (event.button == 2) {
+        setIsMenuOpen(true);
+      }
+    });
+
+    return () => cardRef.current?.removeEventListener("contextmenu", event);
+  }, []);
+
   return (
-    <Draggable key={task.id} draggableId={`${task.id}`} index={index}>
-      {(draggableProvider, snapshot) => (
-        <Flex
-          flexDirection="column"
-          ref={draggableProvider.innerRef}
-          {...draggableProvider.draggableProps}
-          {...draggableProvider.dragHandleProps}
-          mb="1rem"
-          w="100%"
-          minH="72px"
-          bg="card-bg"
-          rounded="0.5rem"
-          p="1.5rem"
-          justifyContent="flex-start"
-          position="relative"
-          gap="1rem"
-          cursor="default"
-        >
-          <Flex justifyContent="space-between">
-            <PriorityBox handlePriority={handlePriority} task={task} />
-            <DisplayText
-              handleValue={handleValue}
-              handleIsEditable={handleIsEditable}
-              isEditable={isEditable}
-              task={task}
-              value={value}
-            />
+    <Box ref={cardRef} className={styles.cardBox}>
+      <MenuContainer
+        isOpen={isMenuOpen}
+        setIsOpen={setIsMenuOpen}
+        deleteTask={deleteTask}
+        task={task}
+        column={column}
+      />
+      <Draggable key={task.id} draggableId={`${task.id}`} index={index}>
+        {(draggableProvider, snapshot) => (
+          <Flex
+            ref={draggableProvider.innerRef}
+            {...draggableProvider.draggableProps}
+            {...draggableProvider.dragHandleProps}
+            className={styles.cardContainer}
+          >
+            <Flex justifyContent="space-between">
+              <PriorityBox handlePriority={handlePriority} task={task} />
+              <DisplayText
+                handleValue={handleValue}
+                handleIsEditable={handleIsEditable}
+                isEditable={isEditable}
+                task={task}
+                value={value}
+              />
+            </Flex>
 
-            <DeleteButton deleteTask={deleteTask} task={task} column={column} />
+            <DateTime dateTime={creationDate} />
           </Flex>
-
-          <DateTime dateTime={creationDate} />
-        </Flex>
-      )}
-    </Draggable>
+        )}
+      </Draggable>
+    </Box>
   );
 };
 

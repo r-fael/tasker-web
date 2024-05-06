bf6bb00b50b7fd7ee1e475c2b4c05787a365c431
rafael medeiros <60578498+r-fael@users.noreply.github.com>
2024-05-06 21:36:28 +0000

    feat: Add rehype

--- src/Components/Markdown/Markdown.js
+++ src/Components/Markdown/Markdown.js
@@ -3,11 +3,14 @@ import React from "react";
 import remarkGfm from "remark-gfm";
 import styles from "./Markdown.module.scss";
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 
 const Markdown = ({ onclick, content }) => {
   return (
     <Box onClick={onclick} className={styles.reactMarkdownContainer}>
       <ReactMarkdown
+        remarkRehypeOptions={[rehypeRaw]}
+        linkTarget="_blank"
         redenre={{ code: Highlight }}
         className={styles.reactMarkdown}
         remarkPlugins={[remarkGfm]}
